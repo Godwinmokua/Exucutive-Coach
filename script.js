92a492a82ae6d9980190cc10d34175395687978c
Godwin Mokua <godwinmokua2004@gmail.com>
2025-03-03 19:08:42 +0300

    final

--- script.js
+++ script.js
@@ -2,40 +2,36 @@
 function openloginpage(){
      document.getElementById("login-container").style.display = "block";
      document.getElementById("register-container").style.display = "none";
-     document.body.classList.add("no-scroll");
-     document.getElementById("forgot-container").style.display = "none";
+     document.getElementById("homepage").classList.add("no-scroll");
 }
 function closeloginpage(){
      document.getElementById("login-container").style.display = "none";
-     document.body.classList.remove("no-scroll");
+     document.getElementById("homepage").classList.remove("no-scroll");
 }
 
 // REGISTER PAGE
 function openregister() {
      document.getElementById("register-container").style.display = "block";
-     document.body.classList.add("no-scroll");
+     document.getElementById("homepage").classList.add("no-scroll");
      document.getElementById("login-container").style.display = "none";
-   }
-   function closeregister() {
+}
+function closeregister() {
      document.getElementById("register-container").style.display = "none";
-     document.body.classList.remove("no-scroll");
+     document.getElementById("homepage").classList.remove("no-scroll");
 }
 
    
 // FORGOT PASSWORD 
 function openforgotpass() {
      document.getElementById("forgot-container").style.display = "block";
-     document.body.classList.add("no-scroll");
+     document.getElementById(homepage).classList.add("no-scroll");
      document.getElementById("login-container").style.display = "none";
 
 }
 function closeforgotpass() {
-     setTimeout(() => {
-          document.getElementById("forgot-container").style.display = "none";
-          document.body.classList.remove("no-scroll");
-          location.reload(); // Reload the page after closing the popup
-        }, 300);
-     // document.body.remove("no-scroll");
+     document.getElementById("forgot-container").style.display = "none";
+     document.getElementById("homepage").classList.remove("no-scroll");
+     location.reload(); // Reload the page after closing the popup
 }
    
    
