36999a6a2978ae50bbc2d7dada4e585dd07971f1
Godwin Mokua <godwinmokua2004@gmail.com>
2025-03-03 19:30:18 +0300

    final

--- script.js
+++ script.js
@@ -19,17 +19,17 @@ function closeregister() {
      document.getElementById("register-container").style.display = "none";
      document.getElementById("homepage").classList.remove("no-scroll");
 }
-
    
 // FORGOT PASSWORD 
 function openforgotpass() {
      document.getElementById("forgot-container").style.display = "block";
-     document.getElementById(homepage).classList.add("no-scroll");
+     document.getElementById("homepage").classList.add("no-scroll");
      document.getElementById("login-container").style.display = "none";
 
 }
 function closeforgotpass() {
      document.getElementById("forgot-container").style.display = "none";
+     document.getElementById("login-container").style.display = "none";
      document.getElementById("homepage").classList.remove("no-scroll");
      location.reload(); // Reload the page after closing the popup
 }
