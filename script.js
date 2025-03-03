cfef3e7eb21d27e256a017e03a4c2d8840887e20
Godwin Mokua <godwinmokua2004@gmail.com>
2025-03-03 18:52:10 +0300

    final

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 // LOGIN PAGE
 function openloginpage(){
-     document.getElementById("login-container").style.display = "flex";
-     setTimeout(() => {
-          document.getElementById("login-container").classList.add("active");
-     }, 10);
+     document.getElementById("login-container").style.display = "block";
      document.getElementById("register-container").style.display = "none";
      document.body.classList.add("no-scroll");
+     document.getElementById("forgot-container").style.display = "none";
 }
 function closeloginpage(){
      document.getElementById("login-container").style.display = "none";
-     setTimeout(() => {
-          document.getElementById("login-container").style.display = "none";
-        }, 300);
      document.body.classList.remove("no-scroll");
 }
 
 // REGISTER PAGE
 function openregister() {
-     document.getElementById("register-container").style.display = "flex";
+     document.getElementById("register-container").style.display = "block";
      document.body.classList.add("no-scroll");
      document.getElementById("login-container").style.display = "none";
    }
@@ -29,7 +24,7 @@ function openregister() {
    
 // FORGOT PASSWORD 
 function openforgotpass() {
-     document.getElementById("forgot-container").style.display = "flex";
+     document.getElementById("forgot-container").style.display = "block";
      document.body.classList.add("no-scroll");
      document.getElementById("login-container").style.display = "none";
 
