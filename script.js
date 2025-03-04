edde949f3d9312f4af2975b85b4ac1275eb2ff74
Ogbakidavid <creativeogbaki04@gmail.com>
2025-03-04 21:26:20 +0100

    change wrong codes

--- script.js
+++ script.js
@@ -5,35 +5,35 @@ setInterval(()=>{
 },300000);
 
 // LOGIN PAGE
-function openloginpage(){
+function openLoginPage(){
      document.getElementById("login-container").style.display = "block";
      document.getElementById("register-container").style.display = "none";
      document.getElementById("homepage").classList.add("no-scroll");
 }
-function closeloginpage(){
+function closeLoginPage(){
      document.getElementById("login-container").style.display = "none";
      document.getElementById("homepage").classList.remove("no-scroll");
 }
 
 // REGISTER PAGE
-function openregister() {
+function openRegister() {
      document.getElementById("register-container").style.display = "block";
      document.getElementById("homepage").classList.add("no-scroll");
      document.getElementById("login-container").style.display = "none";
 }
-function closeregister() {
+function closeRegister() {
      document.getElementById("register-container").style.display = "none";
      document.getElementById("homepage").classList.remove("no-scroll");
 }
    
 // FORGOT PASSWORD 
-function openforgotpass() {
+function openForgotPass() {
      document.getElementById("forgot-container").style.display = "block";
      document.getElementById("homepage").classList.add("no-scroll");
      document.getElementById("login-container").style.display = "none";
 
 }
-function closeforgotpass() {
+function closeForgotPass() {
      document.getElementById("forgot-container").style.display = "none";
      document.getElementById("login-container").style.display = "none";
      document.getElementById("homepage").classList.remove("no-scroll");
